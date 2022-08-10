aeb7e7df24c58261c20a0232abab145c9f9a9fc2
Igor40ivskyi <sorokivskyi777@gmail.com>
2022-08-10 10:47:22 +0300

    add lesson 4

--- practice/main.js
+++ practice/main.js
@@ -1,1 +1,69 @@
-console.log(+433242+234234+34234525 + '' +2354345+234234+234234+2342345+23432+234234+34534)
+// let gangsters = [
+//     {name: 'Vasya', age: 31, status: false},
+//     {name: 'Lesyk', age: 33, status: true},
+//     {name: 'Tiutya', age: 34, status: false},
+//     {name: 'Petya', age: 22, status: true},
+//     {name: 'Oleg', age: 44, status: false},
+//     {name: 'Marty', age: 66, status: true},
+//     {name: 'Darly', age: 20, status: true},
+//     {name: 'Sara', age: 35, status: false},
+//     {name: 'Bewerley', age: 58, status: false},
+//     {name: 'Alex', age: 64, status: true},
+// ];
+// for (let gangster of gangsters) {
+//     document.write(gangster);
+// }
+
+// let groceries = [
+//     {
+//         name: 'milk',
+//         price: 33,
+//         image: 'https://p.turbosquid.com/ts-thumb/7I/XMLwAZ/Sa/image1/png/1615465799/600x600/fit_q87/3758818266e09dfa2b694a9ec363dbbaf5388e2c/image1.jpg'
+//     },
+//     {
+//         name: 'water',
+//         price: 10,
+//         image: 'https://s7d1.scene7.com/is/image/mcdonalds/t-mcdonalds-Dasani-Water:1-3-product-tile-desktop?wid=765&hei=472&dpr=off'
+//     },
+//     {
+//         name: 'juice',
+//         price: 48,
+//         image: 'https://www.minutemaid.com/content/dam/nagbrands/us/minutemaidus/en/products/variety-juices-and-more/jtg/jtg-og-image.png'
+//     },
+//     {
+//         name: 'tea',
+//         price: 90,
+//         image: 'https://thumb.tildacdn.com/tild6262-3430-4434-b363-306366646230/-/format/webp/White_Berries.png'
+//     },
+//     {
+//         name:'cola',
+//         price:15,
+//         image:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS1xGOhKZKYcSbsNbzmFtZiAFESf2cGLz97fw&usqp=CAU'
+//     }
+// ];
+// for (let grocery of groceries) {
+//     if (grocery.price > 35) {
+//         document.write(`
+//         <div>
+//                 <h1>${grocery.name} ${grocery.price}</h1>
+//                 <img src="${grocery.image}" alt="">
+//         </div>`)
+//     }
+// }
+
+// let gangsters = [
+//     {name: 'Vasya', age: 31, status: false},
+//     {name: 'Lesyk', age: 33, status: true},
+//     {name: 'Tiutya', age: 34, status: false},
+//     {name: 'Petya', age: 22, status: true},
+//     {name: 'Oleg', age: 44, status: false},
+//     {name: 'Marty', age: 66, status: true},
+//     {name: 'Darly', age: 20, status: true},
+//     {name: 'Sara', age: 35, status: false},
+//     {name: 'Bewerley', age: 58, status: false},
+//     {name: 'Alex', age: 64, status: true},
+// ];
+// for (let f = 0;f < gangsters.length;f++) {
+// let gangster = gangsters[f];
+//     console.log(gangster);
+// }
